fca9db369c09a1ede061df45c367ae6d4c1a0168
jiaanqiang <32977679+jiaanqiang@users.noreply.github.com>
2025-10-16 14:28:23 +0800

    Update 4CNN_BiLGRU_Attention_train.py

--- Script/4CNN_BiLGRU_Attention_train.py
+++ Script/4CNN_BiLGRU_Attention_train.py
@@ -13,14 +13,14 @@ from sklearn.metrics import matthews_corrcoef
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from sklearn.metrics import roc_auc_score, roc_curve
-from torch.optim.lr_scheduler import ReduceLROnPlateau
+
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-# 氨基酸到索引的映射（1-20）
+# Mapping of Amino Acids to Indices (1-20)
 amino_to_index = {amino: i+1 for i, amino in enumerate('ARNDCQEGHILKMFPSTWYV')}
 
-# 创建自定义数据集类
+# Custom dataset class
 class ProteinDataset(Dataset):
     def __init__(self, data, amino_to_index, max_length=100):
         self.data = data
@@ -34,7 +34,7 @@ class ProteinDataset(Dataset):
         sequence = self.data.iloc[idx, 0]
         label = self.data.iloc[idx, 1]
 
-        # 将氨基酸序列转换为索引序列，并填充到指定的最大长度
+        # Convert amino acid sequence to index sequence
         sequence_indices = [self.amino_to_index.get(amino, 0) for amino in sequence]
         sequence_indices += [0] * (self.max_length - len(sequence_indices))
         sequence_indices = sequence_indices[:self.max_length]
@@ -44,16 +44,14 @@ class ProteinDataset(Dataset):
             'label': torch.tensor(label)
         }
 
-# 从本地CSV文件中读取数据
-data = pd.read_csv('At_Rice_all_1vs100.csv')
+# Read data from a local CSV file
+data = pd.read_csv('Data_1vs1.csv')
 
-device = torch.device('cuda')
+# Separating features (sequences) and labels
+X = data.iloc[:, 0]  # The first column is the sequence
+y = data.iloc[:, 1]  # The second column is the label
 
-# 分离特征（序列）和标签
-X = data.iloc[:, 0]  # 第一列是序列
-y = data.iloc[:, 1]  # 第二列是标签
-
-# 进行分层划分，确保两个类别的比例在训练集和测试集中保持一致
+# Split the data ensuring consistent class ratios in train and test sets
 train_X_1, test_X_1, train_y_1, test_y_1 = train_test_split(X[y == 1], y[y == 1], test_size=0.2, random_state=100)
 train_X_0, test_X_0, train_y_0, test_y_0 = train_test_split(X[y == 0], y[y == 0], test_size=0.2, random_state=100)
 
@@ -62,22 +60,22 @@ test_X = pd.concat([test_X_1, test_X_0])
 train_y = pd.concat([train_y_1, train_y_0])
 test_y = pd.concat([test_y_1, test_y_0])
 
-# 组合训练集和测试集的特征和标签，然后在每个类别内部对数据进行随机洗牌，增加数据随机性
+# Combine features and labels for train and test sets and shuffle the data within each class to add randomness
 train_data = shuffle(pd.DataFrame({'sequence': train_X, 'label': train_y}), random_state=100)
 test_data = shuffle(pd.DataFrame({'sequence': test_X, 'label': test_y}), random_state=100)
 
-# 创建训练集和测试集的数据集对象
+# Create dataset objects for train and test sets
 train_dataset = ProteinDataset(train_data, amino_to_index=amino_to_index, max_length=100)
 test_dataset = ProteinDataset(test_data, amino_to_index=amino_to_index, max_length=100)
 
-# 创建数据加载器
+# Create data loaders
 batch_size = 64
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-# 定义模型
 
 
-# 定义模型
+
+# Define model
 class ProteinPredictor(nn.Module):
     def __init__(self, input_size, num_classes, max_length=100):
         super(ProteinPredictor, self).__init__()
@@ -87,7 +85,7 @@ class ProteinPredictor(nn.Module):
         
         # Define the layers
         self.embedding = nn.Embedding(input_size, embedding_dim)
-        self.conv1 = nn.Conv1d(embedding_dim, 256, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv1d(embedding_dim, 256, kernel_size=5, padding=2)
         self.conv2 = nn.Conv1d(256, 512, kernel_size=3, padding=1)
         self.conv3 = nn.Conv1d(512, 1024, kernel_size=3, padding=1)
         self.conv4 = nn.Conv1d(1024, 2048, kernel_size=3, padding=1)
@@ -95,7 +93,7 @@ class ProteinPredictor(nn.Module):
         self.dropout = nn.Dropout(0.2)
         # Adjust the input size for GRU to match the output size of convolutions
         self.bigru = nn.GRU(2048, hidden_size, num_layers=6, batch_first=True, bidirectional=True, dropout=0.2)
-        self.self_attention = nn.MultiheadAttention(embed_dim=hidden_size * 2, num_heads=16, dropout=0.2)
+        self.self_attention = nn.MultiheadAttention(embed_dim=hidden_size * 2, num_heads=8, dropout=0.2)
         self.fc = nn.Linear(hidden_size * 2, num_classes)
         self.relu = nn.ReLU()
         
@@ -117,10 +115,10 @@ class ProteinPredictor(nn.Module):
         x = self.conv2(x)
         x = self.relu(x)
         x = self.maxpool(x)
-        x = self.conv3(x)  # 新增的卷积层
+        x = self.conv3(x)
         x = self.relu(x)
         x = self.maxpool(x)
-        x = self.conv4(x)  # 新增的卷积层
+        x = self.conv4(x)
         x = self.relu(x)
         x = self.maxpool(x)
         
@@ -137,46 +135,34 @@ class ProteinPredictor(nn.Module):
         
         return output
 
-# 初始化模型
+# Initialize model
 input_size = len(amino_to_index) + 1
 num_classes = 2
 
+device = torch.device('cuda')
 
 model = ProteinPredictor(input_size, num_classes)
 model.to(device)  # Move the model to GPU if available
 
+# Define optimizer and loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-5)
 
-# 定义学习率调度器参数
-initial_lr = 0.00001  # 初始学习率，您可以根据需要调整
-lr_decay_factor = 0.5
-lr_patience = 4
-current_lr = initial_lr
-no_improvement_count = 0
+#  Define learning rate scheduler parameters
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2, factor=0.5, verbose=True) #ReduceLROnPlateau
 
-# 定义优化器和损失函数
-optimizer = torch.optim.AdamW(model.parameters(), lr=initial_lr, weight_decay=1e-5)
-
-class_weight = torch.tensor([0.008, 1.0])  # 根据类别不平衡情况调整权重
-criterion = torch.nn.CrossEntropyLoss(weight=class_weight.to(device))
-
-model.to(device)
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, roc_curve, precision_recall_curve, average_precision_score, matthews_corrcoef, confusion_matrix
-
-# 定义学习率调度器
-lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=lr_decay_factor, patience=lr_patience, verbose=True)
-
-# 训练和测试循环
+# Training and testing loops
 num_epochs = 30
-best_roc_auc = 0.0  # Initialize best sensitivity
+best_accuracy = 0.0
+all_labels = []
+all_scores = []
 
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0.0
-
     for batch in train_dataloader:
-        sequence_indices = batch['sequence_indices'].to(device)
-        labels = batch['label'].to(device)
+        sequence_indices = batch['sequence_indices'].to(device)  # Move data to GPU
+        labels = batch['label'].to(device)  # Move data to GPU
 
         optimizer.zero_grad()
         outputs = model(sequence_indices)
@@ -187,80 +173,64 @@ for epoch in range(num_epochs):
         total_loss += loss.item()
 
     avg_loss = total_loss / len(train_dataloader)
-    # 验证循环
+
+    # Validation Loop
     model.eval()
-    true_labels = []
+    true_labels = []  # Initialize true_labels and predicted_labels lists
     predicted_labels = []
-    all_labels = []
-    all_scores = []
+    correct = 0
+    total = 0
 
     with torch.no_grad():
         for batch in test_dataloader:
-            sequence_indices = batch['sequence_indices'].to(device)
-            labels = batch['label'].to(device)
+            sequence_indices = batch['sequence_indices'].to(device)  # Move data to GPU
+            labels = batch['label'].to(device)  # Move data to GPU
 
             outputs = model(sequence_indices)
-            _, predicted = torch.max(outputs, 1)
-
+            _, predicted = torch.max(outputs.data, 1)
+            
             true_labels.extend(labels.cpu().numpy())
             predicted_labels.extend(predicted.cpu().numpy())
-
+            
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
             probabilities = torch.nn.functional.softmax(outputs, dim=1)[:, 1]
+
             all_labels.extend(labels.cpu().numpy())
             all_scores.extend(probabilities.cpu().numpy())
 
-    # 调整阈值
-    threshold = 0.5  # 你可以根据需要调整这个阈值
+        auc = roc_auc_score(all_labels, all_scores)
 
-    predicted_labels_adjusted = (np.array(all_scores) >= threshold).astype(int)
-    # 计算 AUC-ROC 曲线
-    fpr, tpr, _ = roc_curve(all_labels, all_scores)
-    roc_auc = roc_auc_score(all_labels, all_scores)
+        mcc = matthews_corrcoef(true_labels, predicted_labels)
 
-    # 计算其他评估指标
-    mcc = matthews_corrcoef(true_labels, predicted_labels_adjusted)
-    conf_matrix = confusion_matrix(true_labels, predicted_labels_adjusted)
+        conf_matrix = confusion_matrix(true_labels, predicted_labels)
 
-    tp = conf_matrix[1, 1]
-    tn = conf_matrix[0, 0]
-    fp = conf_matrix[0, 1]
-    fn = conf_matrix[1, 0]
 
-    sn = tp / (tp + fn) if (tp + fn) != 0 else 0.0
-    sp = tn / (tn + fp) if (tn + fp) != 0 else 0.0
-    accuracy = (tp + tn) / (tp + tn + fp + fn)
-    precision = tp / (tp + fp) if (tp + fp) != 0 else 0.0
-    f1_score = 2 * (precision * sn) / (precision + sn) if (precision + sn) != 0 else 0.0
+        #precision = tp / (tp + fp) if (tp + fp) != 0 else 0.0
 
-    print('Epoch [{}/{}]\tAvg Loss: {:.4f}, True Positives: {:.4f}, True Negatives: {:.4f}, False Positives: {:.4f}, False Negatives: {:.4f},'
-        .format(epoch+1, num_epochs, avg_loss, tp, tn, fp, fn))
+        tp = conf_matrix[1, 1]
+        tn = conf_matrix[0, 0]
+        fp = conf_matrix[0, 1]
+        fn = conf_matrix[1, 0]
 
-    print('Epoch [{}/{}]\tTrain Loss: {:.4f}\tTest Accuracy: {:.2f}%, AUC: {:.4f}, Sn: {:.4f}, Sp: {:.4f}, Mcc: {:.4f}, Precision: {:.4f}, f1_score: {:.4f}'
-        .format(epoch+1, num_epochs, avg_loss, accuracy * 100, roc_auc, sn, sp, mcc, precision, f1_score))
-    
-    # 绘制 AUC-ROC 曲线
-    plt.figure(figsize=(8, 8))
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'AUC = {roc_auc:.2f}')
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend(loc='lower right')
-    plt.savefig(f'roc_curve_epoch_{epoch+1}.pdf')  # 保存为 PDF 文件
-    plt.close()
-    # 保存在sn最好时的模型
-    if roc_auc > best_roc_auc:
-        best_roc_auc = roc_auc
-        no_improvement_count = 0  # Reset the counter when accuracy improves
-        # 保存模型检查点
-        torch.save(model.state_dict(), 'best_CNN_gpu.pth')
-    else:
-        no_improvement_count += 1
-        if no_improvement_count >= lr_patience:
-            current_lr *= lr_decay_factor
-            optimizer = torch.optim.AdamW(model.parameters(), lr=current_lr)
-            lr_scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=lr_decay_factor, patience=lr_patience, verbose=True)
-            print(f'Learning rate reduced to {current_lr}')
+        sn = tp / (tp + fn) if (tp + fn) != 0 else 0.0
+        sp = tn / (tn + fp) if (tn + fp) != 0 else 0.0
 
-    # 更新学习率
-    lr_scheduler.step(roc_auc)
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+
+        precision = tp / (tp + fp) if (tp + fp) != 0 else 0.0
+        f1_score = 2 * (precision * sn) / (precision + sn) if (precision + sn) != 0 else 0.0
+
+        print('Epoch [{}/{}]\tAvg Loss: {:.4f}, True Positives: {:.4f}, True Negatives: {:.4f}, False Positives: {:.4f}, False Negatives: {:.4f},'
+            .format(epoch+1, num_epochs, avg_loss, tp, tn, fp, fn))
+
+        print('Epoch [{}/{}]\tTrain Loss: {:.4f}\tTest Accuracy: {:.2f}%, AUC: {:.4f}, Sn: {:.4f}, Sp: {:.4f}, Mcc: {:.4f}, Precision: {:.4f}, f1_score: {:.4f}'
+            .format(epoch+1, num_epochs, avg_loss, accuracy * 100, auc, sn, sp, mcc, precision,f1_score))
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            # Save the model checkpoint
+            torch.save(model.state_dict(), 'best_model_CNN.pth')
+    #Update the learning rate scheduler
+    scheduler.step(accuracy)
